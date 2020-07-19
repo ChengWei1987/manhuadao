cfc7017719017e59ae5b72835553711127772578
chengwei <1546675132@qq.com>
2020-07-19 22:47:03 +0800

    day01首次提交

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,6 +1,16 @@
 <template>
   <div class="page-home">
-    <h1>首页</h1>
+    <header class="index-header">
+      <a href="mine.html?cpid=0">
+        <div class="header-user">
+              <div class="user-btn"></div>
+        </div>
+      </a>
+      <div class="header-logo"></div>
+      <a href="search.html?cpid=0">
+        <div class="header-search"></div>
+      </a>
+  </header>
   </div>
 </template>
 
@@ -9,3 +19,37 @@ export default {
   name: 'Home'
 }
 </script>
+<style lang="scss" scoped>
+  .page-home {
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+
+    .index-header {
+      display: flex;
+      height: 44px;
+      align-items: center;
+      justify-content: space-between;
+      padding: 0 20px;
+      box-sizing: border-box;
+      .user-btn {
+        width: 25px;
+        height: 25px;
+        background: url('../../assets/images/person.png') no-repeat;
+        background-size: 100%;
+      }
+      .header-logo {
+        width: 92px;
+        height: 28px;
+        background: url(../../assets/logo.png) no-repeat;
+        background-size: 100%;
+      }
+      .header-search {
+        width: 25px;
+        height: 25px;
+        background: url(../../assets/images/search.png) no-repeat;
+        background-size: 100%;
+      }
+    }
+}
+</style>
