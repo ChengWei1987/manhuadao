492f7ee1705ffb8d9012b7ba897b75ba12190d48
chengwei <1546675132@qq.com>
2020-07-21 08:28:41 +0800

    day02更新

---
+++ src/components/Swiper/index.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="swiper-container">
+      <div class="swiper-wrapper">
+        <div class="swiper-slide">Slide 1</div>
+        <div class="swiper-slide">Slide 2</div>
+        <div class="swiper-slide">Slide 3</div>
+      </div>
+      <!-- 如果需要分页器 -->
+      <div class="swiper-pagination"></div>
+    </div>
+</template>
+
+<script>
+import Swiper from 'swiper'
+import 'swiper/swiper.scss'
+
+export default {
+  name: 'Swiper',
+  mounted () {
+    /* eslint-disable */
+    new Swiper(".swiper-container", {
+      pagination: {
+        el: ".swiper-pagination",
+      }
+    });
+    /* eslint-enable */
+  }
+
+}
+</script>
+
+<style lang="scss" >
+  .swiper-container {
+    width: 100%;
+    height: 180px;
+    .swiper-pagination-bullet {
+      opacity: 1;
+      vertical-align: middle;
+      width: 6px;
+      height: 6px;
+      margin: 0 5px;
+      border-radius: 50%;
+      background-color: hsla(0, 0%, 100%, .7);
+    }
+    .swiper-pagination-bullet-active {
+      width: 20px;
+      height: 10px;
+      background: url('../../assets/icon/dot.png') no-repeat;
+      background-size: 100%;
+    }
+  }
+</style>
--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -13,38 +13,24 @@
       </a>
     </header>
     <!-- 首页的头部 结束 -->
-
-    <!-- 轮播图的 开始 -->
-    <div class="swiper-container">
-      <div class="swiper-wrapper">
-        <div class="swiper-slide">Slide 1</div>
-        <div class="swiper-slide">Slide 2</div>
-        <div class="swiper-slide">Slide 3</div>
-      </div>
-      <!-- 如果需要分页器 -->
-      <div class="swiper-pagination"></div>
-    </div>
-    <!-- 轮播图的 结束 -->
+    <!-- 轮播图 开始 -->
+    <Swiper></Swiper>
+    <!-- 轮播图 结束 -->
   </div>
 </template>
 
 <script>
 // 引入swiper的核心和样式
-import Swiper from 'swiper'
-import 'swiper/swiper.scss'
+// 如果使用../相对路径,如果当前组件位置发生改变
+// 那么相对路径也就需要去修改,如果使用@符号别名的方式,后期就不需要修改这个提示
+import Swiper from '@/components/Swiper/index'
 
 export default {
   name: 'Home',
-  mounted () {
-    // 此时不允许出现new关键字但是不得不用,就需要eslint的注释方法来解决
-    /* eslint-disable */
-    new Swiper(".swiper-container", {
-      pagination: {
-        el: ".swiper-pagination"
-      }
-    });
-    /* eslint-enable */
+  components: {
+    Swiper: Swiper
   }
+
 }
 </script>
 <style lang="scss" scoped>
@@ -78,10 +64,6 @@ export default {
       background: url(../../assets/images/search.png) no-repeat;
       background-size: 100%;
     }
-  }
-  .swiper-container {
-    width: 100%;
-    height: 180px;
   }
 }
 </style>
